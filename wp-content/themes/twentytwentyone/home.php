eccc6a7cdcff0e00e92baf2d95e4b8d6c13726f8
Smartdeveloper2025 <smartdeveloper2025@gmail.com>
2021-10-06 13:03:46 +0530

    home page

--- wp-content/themes/twentytwentyone/home.php
+++ wp-content/themes/twentytwentyone/home.php
@@ -12,6 +12,33 @@
 
 get_header();
 
+//calculate ago time from updated_at 
+function time_elapsed_string($date)
+{
+	$timestamp = strtotime($date);	
+	   
+	$strTime = array("second", "minute", "hour", "day", "month", "year");
+	$length = array("60","60","24","30","12","10");
+
+	$currentTime = time();
+	
+	if($currentTime >= $timestamp) {
+		$diff     = time()- $timestamp;
+		for($i = 0; $diff >= $length[$i] && $i < count($length)-1; $i++) {
+		$diff = $diff / $length[$i];
+		}
+
+		$diff = round($diff);
+		return "Last edit: ".$diff . " " . $strTime[$i] . "s ago ";
+	}
+}
+
+global $wpdb;
+$current_user_id = get_current_user_id();
+
+$creation_table = $wpdb->prefix."tbl_creation";
+$creation_result = $wpdb->get_results( "SELECT * from {$creation_table} where user_id = '{$current_user_id}' ", ARRAY_A );
+
 ?>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/template-style.css" type="text/css" />
@@ -24,47 +51,36 @@ get_header();
         <div class="container py-5 px-0">
             <div class="row m-0 creation-row">
                 <h4 class="section-link"><a href="">My Creations</a></h4>
-                <div class="mycreation-box">
-                    <i class="creaton-dot"></i>
-                    <i class="creaton-dot"></i>
-                    <i class="creaton-dot"></i>
-                    <h4 class="creation-heading m-0">SH resume</h4>
-                    <p class="creation-info m-0">Last edit: 2 mints ago</p>
-
-                </div>
-                <div class="mycreation-box">
-                    <i class="creaton-dot"></i>
-                    <i class="creaton-dot"></i>
-                    <i class="creaton-dot"></i>
-                    <h4 class="creation-heading m-0">uni resume</h4>
-                    <p class="creation-info m-0">Last edit: 5 mints ago</p>
-
-                </div>
-                <div class="mycreation-box">
-                    <i class="creaton-dot"></i>
-                    <i class="creaton-dot"></i>
-                    <i class="creaton-dot"></i>
-                    <h4 class="creation-heading m-0">job hunting network</h4>
-                    <p class="creation-info m-0">Last edit: 7 mints ago</p>
-
-                </div>
-                <div class="mycreation-box">
-                    <i class="creaton-dot"></i>
-                    <i class="creaton-dot"></i>
-                    <i class="creaton-dot"></i>
-                    <h4 class="creation-heading m-0">shower thougths</h4>
-                    <p class="creation-info m-0">Last edit: 22 mints ago</p>
-
-                </div>
-                <a href="http://localhost/knomad/edit-topic-notes/?edit_id=1">
-					<div class="mycreation-box">
-						<i class="creaton-dot"></i>
-						<i class="creaton-dot"></i>
-						<i class="creaton-dot"></i>
-						<h4 class="creation-heading m-0">ANTh001 notes</h4>
-						<p class="creation-info m-0">Last edit: 22 mints ago</p>
-					</div>
-				</a>
+				<?php 
+					if(count($creation_result) >= 0){
+						foreach($creation_result as $creation_data){
+							if($creation_data['type'] == 'topic'){
+								$color_class = 'topic_color';
+							} else if($creation_data['type'] == 'network'){
+								$color_class = 'network_color';
+							} else if($creation_data['type'] == 'experience'){
+								$color_class = 'experience_color';
+							} else {
+								$color_class = '';
+							} 
+						?>
+							<a href="http://localhost/knomad/edit-topic-notes/?edit_id=<?php echo $creation_data['id']; ?>">
+							<div class="mycreation-box <?php echo $color_class;  ?>"">
+								<i class="creaton-dot"></i>
+								<i class="creaton-dot"></i>
+								<i class="creaton-dot"></i>
+								<h4 class="creation-heading m-0"><?php echo $creation_data['name']; ?></h4>
+								<p class="creation-info m-0"><?php echo time_elapsed_string($creation_data['updated_at']); ?></p>
+							</div>
+						</a>
+						<?php
+						}
+					} else {
+						echo '<h2 class="no_data">No Data Found</h2>';
+					}
+				?>
+                
+                
             </div>
         </div>
 	</div>
